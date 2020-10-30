4a5c783da57622775660c059c7b579f55f84a838
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-10-30 13:50:42 +0100

    minor fixes

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -20,10 +20,10 @@ export const NavBar = () => {
                                 <a className="nav-link" href="#top" style ={{color: "#F5772F"}} >LEARNABLE <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="top#">AGORA</a>
+                                <a className="nav-link" href="#top">AGORA</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="top#">STARTZONE</a>
+                                <a className="nav-link" href="#top">STARTZONE</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#top">ABOUT</a>
